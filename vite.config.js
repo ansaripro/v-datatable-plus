87c8fc0b1b7251ee4877025d03495ca66a0f426f
ansaripro <adnanansari.pro@gmail.com>
2024-02-07 13:21:53 -0500

    Package json correction

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    exclude: [
+      'vuetify',
+    ],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
