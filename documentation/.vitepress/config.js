c8eb338f610f89fded6bfd63ab9ab24650be168e
ansaripro <adnanansari.pro@gmail.com>
2024-05-30 06:48:46 -0400

    Vitepress base path update

--- documentation/.vitepress/config.js
+++ documentation/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/v-datatable-plus/',
   title: 'Vuetify DataTable Plus',
   description: 'Documentation for Vuetify Datatable Plus',
   themeConfig: {
